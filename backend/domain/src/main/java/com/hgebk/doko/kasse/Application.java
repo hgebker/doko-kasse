234e71528ae6e4f9cd4029a11cb9c2da65c86936
Hannes Gebker <hannibal2311@googlemail.com>
2024-08-28 07:47:49 +0200

    Fix build - merge app auto config - add run config

--- backend/domain/src/main/java/com/hgebk/doko/kasse/Application.java
+++ backend/domain/src/main/java/com/hgebk/doko/kasse/Application.java
@@ -1,14 +1,10 @@
 package com.hgebk.doko.kasse;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
-@ComponentScan(basePackages = { "com.hgebk.doko.kasse" })
+@SpringBootApplication(scanBasePackages = { "com.hgebk.doko.kasse" }, exclude = { DataSourceAutoConfiguration.class })
 public class Application {
 
     public static void main(String[] args) {
